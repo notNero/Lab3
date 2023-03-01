d2512b739374b82745c804642a94f499d6111c4d
notNero <kronat83@gmail.com>
2023-03-01 15:18:58 +0300

    Changed NavigationScreen.kt, ScrollingScreen.kt and strings.xml

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ScrollingScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ScrollingScreen.kt
@@ -1,9 +1,13 @@
 package com.raywenderlich.android.lab1.screens
 
+import android.annotation.SuppressLint
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
@@ -11,10 +15,10 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
-
 @Composable
 fun ScrollingScreen() {
     MyScrollingScreen()
@@ -25,16 +29,21 @@ fun ScrollingScreen() {
 }
 
 @Composable
-fun MyScrollingScreen() {
-    //TODO add your code here
-}
-
-@Composable
-fun BookImage(@DrawableRes imageResId: Int, @StringRes contentDescriptionResId: Int) {
+fun BookImage(@DrawableRes imageResId: Int,@StringRes contentDescriptionResId: Int) {
     Image(
         bitmap = ImageBitmap.imageResource(imageResId),
         contentDescription = stringResource(contentDescriptionResId),
-    contentScale = ContentScale.FillBounds,
-    modifier = Modifier.size(476.dp, 616.dp )
+        contentScale = ContentScale.FillBounds,
+        modifier = Modifier.size(476.dp, 616.dp )
     )
+}
+
+@SuppressLint("SupportAnnotationUsage")
+@Composable
+fun MyScrollingScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
+        BookImage(R.drawable.advanced_architecture_android, R.string.architecture)
+        BookImage(R.drawable.kotlin_aprentice, R.string.learn_kotlin)
+        BookImage(R.drawable.kotlin_coroutines, R.string.coroutines)
+    }
 }
